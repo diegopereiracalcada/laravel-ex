d936cc325920cf2bce70bf1f652c8d087680c758
diegopereiracalcadadev <diegopereiracalcada@gmail.com>
2020-02-26 15:47:29 -0300

    atualizando database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ClientesTableSeeder::class);
         $this->call(Clientes2TableSeeder::class);
+        $this->call(Clientes4TableSeeder::class);
         $this->call(ChamadosTableSeeder::class);
         $this->call(CategoriasTableSeeder::class);
         $this->call(NotasTableSeeder::class);
