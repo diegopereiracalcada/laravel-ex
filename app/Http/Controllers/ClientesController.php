182aa5318e60f12bc30d139f23b9ca7eee63e68b
diegopereiracalcadadev <diegopereiracalcada@gmail.com>
2020-11-06 22:30:57 -0300

    Front-End funcionando

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -3,59 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Cliente;
-use App\EmailCobranca;
+use App\MailService;
 use App\Nota;
+use App\Services\CobrancaService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Mail;
 
 class ClientesController extends Controller
 {
-    const PREFIXO_BOLETO = "BOLETO";
-    const PREFIXO_NOTA = "NFSe";
 
-    public function getNomeArquivo($prefixo, $cliente){
-        $files = Storage::files();
+    public function cobranca()
+    {
+        $emailsCobranca = (new CobrancaService())->getCobrancasDoMes();
         
-        $fitlered_files = array_filter($files, function($str) use ($prefixo, $cliente){
-            return strpos($str, $prefixo) !== false
-                    && strpos($str, $cliente->shortname) !== false;
-        });
+        return view('cobranca', compact('emailsCobranca'));
+    }
 
-        return implode($fitlered_files);
+    public function enviarEmails(){
+        // dd("Enviando emails...");
+        // (new MailService())->sendFechamento("", "", "000");
+        Mail::send("emails.cobranca", [], function ($message) {
+            $message
+                ->from("clickticonsultoria@gmail.com", "ClickTI Informática")
+                ->to("clickticonsultoria@gmail.com", "Teste cobranca")
+                ->subject("Assunto de teste  - cobranca");
+        });
+        return "Email enviado";
     }
-    
-    public function getNomeArquivoBoleto($cliente){
-        return $this->getNomeArquivo(self::PREFIXO_BOLETO, $cliente);
-    }
-    
-    public function getNomeArquivoNota($cliente){
-        return $this->getNomeArquivo(self::PREFIXO_NOTA, $cliente);
-    }
-    
+
     public function index()
     {
-        $emailsCobranca = [];
-        $clientes = Cliente::where('status_cliente', 'ATIVO')
-        ->orderBy('shortname')
-        ->get();
-
-        foreach ($clientes as $cliente) {
-            if($cliente->shortname == 'CAPI-IPANEMA'
-                || $cliente->shortname == 'AVULSO')
-                continue;
-                
-            $emailCobranca = new EmailCobranca();
-            $emailCobranca->shortname = $cliente->shortname;
-            $emailCobranca->emailDestinatario = $cliente->email;
-            $emailCobranca->nomeArquivoBoleto = $this->getNomeArquivoBoleto($cliente);
-            $emailCobranca->nomeArquivoNota = $this->getNomeArquivoNota($cliente);
-
-            array_push($emailsCobranca, $emailCobranca);
-        }
-
-
-        return $emailsCobranca;
+        return $clientes = Cliente::where('status_cliente', 'ATIVO')
+            ->orderBy('shortname')
+            ->get();
     }
 
     public function create()
